830dac11f8e0aeb9aa2a95436e82020d551ef6c7
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2013-04-03 12:38:24 +0200

    Bind left/right arrows to switch instaread views Keep instaread as iframe in page

--- instaread/init.php
+++ instaread/init.php
@@ -26,26 +26,39 @@ class instaread extends Plugin {
 		$rv = "<img src=\"plugins/instaread/instaread.png\"
 			class='tagsPic' style=\"cursor : pointer\"
 			onclick=\"viewArticleinInstapaper($article_id)\"
+			onload=\"INSTAPAPER_ID=$article_id\"
 			title='".__('Text only view of article.')."'>";
 
 		return $rv;
 	}
 
 	function getInfo() {
-		$id = db_escape_string($this->link, $_REQUEST['id']);
-
-		$result = db_query($this->link, "SELECT title, link
+		$title = "";
+		$article_link = "";
+		$id = "";
+		if( is_numeric($_REQUEST['id']) ) {
+			$result = db_query($this->link, "SELECT title, link
 				FROM ttrss_entries, ttrss_user_entries
-				WHERE id = '$id' AND ref_id = id AND owner_uid = " .$_SESSION['uid']);
-
-		if (db_num_rows($result) != 0) {
-			$title = truncate_string(strip_tags(db_fetch_result($result, 0, 'title')),
-				100, '...');
-			$article_link = db_fetch_result($result, 0, 'link');
+				WHERE id = '".$_REQUEST["id"]."' ".
+				"AND ref_id = id AND owner_uid = " .
+					$_SESSION['uid']);
+			if (db_num_rows($result) != 0) {
+				$title = truncate_string(
+					strip_tags(
+						db_fetch_result($result,
+								0,
+								'title')
+						),
+					100, '...');
+				$article_link = db_fetch_result($result,
+								0,
+								'link');
+			}
 		}
-
-		print json_encode(array("title" => $title, "link" => $article_link,
-				"id" => $id));
+		print json_encode(array(
+			"title" => $title,
+			"link" => $article_link,
+			"id" => $id));
 	}
 
 
--- instaread/instaread.js
+++ instaread/instaread.js
@@ -1,15 +1,41 @@
+$(document).observe('keydown', function (e) {
+  switch (e.keyCode) {
+    case Event.KEY_LEFT:
+      e.stop(); // prevent the default action, like horizontal scroll 
+      viewArticleinInstapaper(); 
+      break;
+    case Event.KEY_RIGHT:
+      e.stop();
+      viewArticleinInstapaper();
+    break;
+  }
+}); 
+var INSTAPAPER_ID = false;
+
+function getHeight(id) {
+	var tot = $("content-insert").getDimensions().height;
+	var hdr = $("POSTHDR-" + id).getDimensions().height;
+	return tot - hdr;
+}
+
+var INSTARED = INSTARED ? INSTARED : new Array();
 	function viewArticleinInstapaper(id) {
+        if( !id ) {
+          id = INSTAPAPER_ID;
+        }
+	if( !id ) {
+	  return;
+	}
 	try {
 		var query = "?op=pluginhandler&plugin=instaread&method=getInfo&id=" + param_escape(id);
-
-		console.log(query);
 
 		var d = new Date();
       var ts = d.getTime();
 
-		var w = window.open('backend.php?op=backend&method=loading', 'ttrss_instaread',
-			"status=0,toolbar=0,location=0,width=600,height=600,scrollbars=1,menubar=0");
-
+//		var w = window.open('backend.php?op=backend&method=loading', 'ttrss_instaread',
+//			"status=0,toolbar=0,location=0,width=600,height=600,scrollbars=1,menubar=0");
+		if(INSTARED[id] != true) {
+		INSTARED[id] = true;
 		new Ajax.Request("backend.php",	{
 			parameters: query,
 			onComplete: function(transport) {
@@ -17,11 +43,35 @@
 
 				var share_url = "http://www.instapaper.com/text?" + "u=" + param_escape(ti.link);
 
-				w.location.href = share_url;
-
+				//w.location.href = share_url;
+				var div = $("INSTARED-" + id);
+				if(div == null) {
+					$$("div.postContent")[0]
+					.parentNode
+					.appendChild(
+						document.createElement("div"));
+					div = $$("div.postContent")[0]
+						.nextSibling;
+					div.setAttribute("id", 
+							"INSTARED-" + id);
+				}
+				$$("div.postContent")[0].hide();
+				div.innerHTML = "";
+				var iframe = document.createElement("iframe");
+				iframe.setAttribute("src", share_url);
+				iframe.setAttribute("height", getHeight(id));
+				iframe.setAttribute("width", "90%");
+				div.appendChild(iframe);
+				div.show();
 			} });
 
-
+		} else {
+			INSTARED[id] = false;
+			if( $("INSTARED-" + id ) != null ) {
+				$("INSTARED-" + id ).hide();
+			}
+                        $$("div.postContent")[0].show();
+		}
 	} catch (e) {
 		exception_error("InstaRead", e);
 	}
